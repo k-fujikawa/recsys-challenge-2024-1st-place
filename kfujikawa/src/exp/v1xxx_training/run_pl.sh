1472f17fb57781e6117cdaf4c1832c29e61e3501
Kazuki Fujikawa <k.fujikawa1014@gmail.com>
2024-06-23 19:31:16 +0900

    Fix run.sh

--- kfujikawa/src/exp/v1xxx_training/run_pl.sh
+++ kfujikawa/src/exp/v1xxx_training/run_pl.sh
@@ -10,5 +10,5 @@ DEVICE=0
 MODEL=$EXP_DIR/v1xxx_training/v1184_111_PL_bert_L4_256d.py
 poetry run python $MODEL train common.fold=0 trainer.devices=[${DEVICE}] common.overwrite=True
 poetry run python $MODEL train common.fold=2 trainer.devices=[${DEVICE}] common.overwrite=True
-poetry run python $MODEL predict --split valid common.fold=0 trainer.devices=[${DEVICE}]
+poetry run python $MODEL predict --split validation common.fold=0 trainer.devices=[${DEVICE}]
 poetry run python $MODEL predict --split test common.fold=2 trainer.devices=[${DEVICE}]
--- kfujikawa/src/exp/v1xxx_training/run_scratch.sh
+++ kfujikawa/src/exp/v1xxx_training/run_scratch.sh
@@ -5,14 +5,14 @@ EXP_DIR=$(cd $(dirname $0)/..; pwd)
 # Base datasets
 MODELS=(
     $EXP_DIR/v1xxx_training/v1157_111_fix_past_v2.py
-    $EXP_DIR/v1xxx_training//v1170_111_L8_128d.py
-    $EXP_DIR/v1xxx_training//v1174_111_L8_128d_smpl3_drophist.py
+    $EXP_DIR/v1xxx_training/v1170_111_L8_128d.py
+    $EXP_DIR/v1xxx_training/v1174_111_L8_128d_smpl3_drophist.py
 )
 DEVICE=0
 
 for MODEL in ${MODELS[@]}; do
     poetry run python $MODEL train common.fold=0 trainer.devices=[${DEVICE}] common.overwrite=True
     poetry run python $MODEL train common.fold=2 trainer.devices=[${DEVICE}] common.overwrite=True
-    poetry run python $MODEL predict --split valid common.fold=0 trainer.devices=[${DEVICE}]
+    poetry run python $MODEL predict --split validation common.fold=0 trainer.devices=[${DEVICE}]
     poetry run python $MODEL predict --split test common.fold=2 trainer.devices=[${DEVICE}]
 done
