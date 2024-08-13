a2551696652414990a7029980a5e672535ab9cc8
Kazuki Fujikawa <k.fujikawa1014@gmail.com>
2024-08-13 23:30:36 +0900

    Update run_scratch.sh

--- kfujikawa/src/exp/v1xxx_training/run_scratch.sh
+++ kfujikawa/src/exp/v1xxx_training/run_scratch.sh
@@ -4,7 +4,7 @@ EXP_DIR=$(cd $(dirname $0)/..; pwd)
 
 # Base datasets
 MODELS=(
-    # $EXP_DIR/v1xxx_training/v1157_111_fix_past_v2.py
+    $EXP_DIR/v1xxx_training/v1157_111_fix_past_v2.py
     $EXP_DIR/v1xxx_training/v1170_111_L8_128d.py
     $EXP_DIR/v1xxx_training/v1174_111_L8_128d_smpl3_drophist.py
 )
